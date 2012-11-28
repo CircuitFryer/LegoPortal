922d4ef5dd9040764edd48cea3618b5930d218ee
Justin Phillips <az33255@umbc.edu>
2012-11-28 18:17:38 -0500

    update

--- admin/template.php
+++ admin/template.php
@@ -1,13 +1,13 @@
 <html>
 <head>
 <title><?php echo $pageTitle; ?></title>
-<link href="./include/admin.css" rel="stylesheet" type="text/css">
+<link href="./admin.css" rel="stylesheet" type="text/css">
 <script language="JavaScript" type="text/javascript" src="../library/common.js"></script>
 <?php
 $n = count($script);
 for ($i = 0; $i < $n; $i++) {
 	if ($script[$i] != '') {
-		echo '<script language="JavaScript" type="text/javascript" src="../include/' . $script[$i]. '"></script>';
+		echo '<script language="JavaScript" type="text/javascript" src="' . $script[$i] . '"></script>';
 	}
 }
 ?>
